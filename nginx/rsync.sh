47d4559be7dcd785d6bf7aabf72f78a2198d655e
vmJ72QiH <52655474+vmJ72QiH@users.noreply.github.com>
2019-08-23 14:46:14 +0800

    Create rsync.sh

---
+++ nginx/rsync.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+#rsync免密传输要先做单向免密
+function nginx_restart(){
+ssh $i "/usr/sbin/nginx -t && systemctl restart nginx"
+if [ $? !=  0 ];then
+        echo -e "\033[1;31m\t\t $i nginx 未重启\033[0m"
+else
+        echo -e "\033[1;32m\t\t$i nginx 重启成功\033[0m"
+fi
+}
+for i in `cat serverlist.txt`
+do
+rsync -avz /etc/nginx/ $i:/etc/nginx/
+nginx_restart
+done
